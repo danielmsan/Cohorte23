810177d3e7a0a8bf40944bd6d9136a25d84761fa
daniel-ms <dani528r@hotmail.com>
2023-03-06 17:30:13 -0600

    JAVA09

---
+++ 05-JAVA/Java-09/oop/src/testers/TestFecha.java
@@ -0,0 +1,18 @@
+package testers;
+
+import clases.Fecha;
+
+public class TestFecha {
+
+	public static void main(String[] args) {
+		Fecha f = new Fecha();
+		f.setDia(6);
+		f.setMes(3);
+		f.setAño(2023);
+		
+		System.out.println(f.diaMesAño());
+		System.out.println("--->"+f);
+		
+	}
+
+}
